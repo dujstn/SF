51c9d7798d7b3c572a050207f45a342a37e4366d
dujstn <du.justinz@gmail.com>
2021-02-01 23:05:22 -0800

    Cleaning:Function to write to .txt file

--- SF 2020-2021/Model Utility Folder/MunicipalityCompare.py
+++ SF 2020-2021/Model Utility Folder/MunicipalityCompare.py
@@ -4,36 +4,45 @@ import numpy as np
 np.set_printoptions(threshold=np.inf)
 
 
+#Function to write city names into .txt files from numpy ndarrays
+
+
+def writeTxt(uniques, name):
+    uniquesString = ";".join(uniques)
+
+    txt = open(os.path.join(os.path.dirname(__file__), name), "w")
+    txt.write(uniquesString)
+    txt.close()
+
+#Function to get a list of unique city names so that they cam be compared
+
+
+def getUniques(data):
+    uniqueCities = pd.Series.unique(data)
+    return uniqueCities
+
+
 # Read data from Insolation & Economics spreadsheets into dataframes
 munInso = pd.read_csv(os.path.join(os.path.dirname(__file__), "Municipality Insolation.csv"))
 curEcon = pd.read_csv(os.path.join(os.path.dirname(__file__), "Current Economics.csv"))
 
+
 # Gather unique names of locations inside Economics spreadsheet
 curEcon_cities = curEcon.pop('Municipality')
 curEcon_uniques = pd.Series.unique(curEcon_cities)
-curEcon_string = ";".join(curEcon_uniques)
-curEconTxt = open(os.path.join(os.path.dirname(__file__), "currentEconomicCities.txt"), "w")
-curEconTxt.write(curEcon_string)
-curEconTxt.close()
+writeTxt(curEcon_uniques, "curEconCities.txt")
+curEconCompare = getUniques(curEcon_cities)
 
 # Gather unique names of locations inside Insolation spreadsheet
 munInso_cities = munInso.pop('Municipality')
 munInso_uniques = pd.Series.unique(munInso_cities)
-munInso_string = ";".join(munInso_uniques)
-munInsoTxt = open(os.path.join(os.path.dirname(__file__), "muniInsolationCities.txt"), "w")
-munInsoTxt.write(munInso_string)
-munInsoTxt.close()
+writeTxt(munInso_uniques, "munInsoCities.txt")
+munInsoCompare = getUniques(munInso_cities)
 
 # Find the locations that aren't common to both sets
-output = np.setdiff1d(curEcon_uniques, munInso_uniques)
-outputString = ";".join(output)
-outputTxt = open(os.path.join(os.path.dirname(__file__), "Notcommon.txt"), "w")
-outputTxt.write(outputString)
-outputTxt.close()
+output = np.setdiff1d(curEconCompare, munInsoCompare)
+writeTxt(output, "OutputOne.txt")
 
 # Compared Economics to Insolation; need to do it the other way around
-outputTwo = np.setdiff1d(munInso_uniques, curEcon_uniques)
-outputStringTwo = ";".join(outputTwo)
-outputTwoTxt = open(os.path.join(os.path.dirname(__file__), "Notcommontwo.txt"), "w")
-outputTwoTxt.write(outputStringTwo)
-outputTwoTxt.close()
+outputTwo = np.setdiff1d(munInsoCompare, curEconCompare)
+writeTxt(outputTwo, "OutputTwo.txt")
