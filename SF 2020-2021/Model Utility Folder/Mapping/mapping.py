243e457fa7f2ce60b3932f5004c9673402c109e1
dujstn <du.justinz@gmail.com>
2021-02-03 16:24:55 -0800

    Added location mapping program

--- SF 2020-2021/Model Utility Folder/Mapping/mapping.py
+++ SF 2020-2021/Model Utility Folder/Mapping/mapping.py
@@ -1,10 +1,35 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import descartes
+from shapely.geometry import Point
 import os
 
-x = os.path.join("SF/SF 2020-2021/Model Utility Folder/Filtering", "FilteredInso.csv")
+# Reading and taking the lat. and long. of unique locaitons
+cities = pd.read_csv(os.path.join(
+    "SF/SF 2020-2021/Model Utility Folder/Filtering", "FilteredEcon.csv"))
+locations = cities[["Municipality", "Latitude", "Longitude"]]
+uniques = locations.drop_duplicates(subset=["Municipality"])
+uniques.to_csv(os.path.join("SF/SF 2020-2021/Model Utility Folder/Mapping",
+                            "UniqueLocations.csv"), index=0)
 
-print(x)
+# Setting up .shp map of Canada
+map = gpd.read_file(os.path.join(
+    "SF/SF 2020-2021/Model Utility Folder/Mapping/Shapefile", "gpr_000b11a_e.shp"))
+fig, ax = plt.subplots(figsize=(15, 15))
 
-y = pd.read_csv(x)
+# Creating location points
+points = pd.read_csv(os.path.join(
+    "SF/SF 2020-2021/Model Utility Folder/Mapping", "UniqueLocations.csv"))
+crs = {"init": "epsg:4326"}
 
-print(y.head())
+geometry = [Point(xy) for xy in zip(points["Longitude"], points["Latitude"])]
+
+# Changing Pandas DataFrame into geoDataFrame
+geo_df = gpd.GeoDataFrame(points, crs=crs, geometry=geometry)
+
+# Creating final map
+map.plot(ax=ax, alpha=0.4, color="grey")
+geo_df.plot(ax=ax, markersize=5, color="blue", marker="o")
+plt.show()
