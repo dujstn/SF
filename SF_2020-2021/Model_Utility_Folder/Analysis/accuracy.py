5bd029dd59c46048ad341d0ca6f37bbd3c28a5c4
dujstn <du.justinz@gmail.com>
2021-02-13 10:53:35 -0800

    Added Power Production Model

--- SF_2020-2021/Model_Utility_Folder/Analysis/accuracy.py
+++ SF_2020-2021/Model_Utility_Folder/Analysis/accuracy.py
@@ -5,14 +5,14 @@ import numpy
 from sklearn.metrics import accuracy_score
 import csv
 
-file = open("SF_2020-2021/Model_Utility_Folder/Analysis/OutputTxts/K10.txt").read()
+file = open("SF_2020-2021/Model_Utility_Folder/Analysis/OutputTxts/B_K2.txt").read()
 z = [list(value) for value in file.split(", ")]
 output = []
 for i in z:
     output.append("".join(i))
 predict = [float(i) for i in output]
 
-evaluate = pd.read_csv("SF_2020-2021/Data/SOUTHCAN_EVAL.csv")
+evaluate = pd.read_csv("SF_2020-2021/Data/B_EVAL.csv")
 expected = evaluate[["First Yr Annual (MWh)"]]
 expectedList = [list(row) for row in expected.values]
 x = []
@@ -27,4 +27,10 @@ for i in range(len(output)):
     listOfAccuracies.append(abs((predict[i]-actuals[i])/actuals[i]*100))
 
 df = pd.DataFrame(data={"Prediction": predict, "Actual": actuals, "Error %": listOfAccuracies})
-df.to_csv(os.path.join("SF_2020-2021/Model_Utility_Folder/Analysis", "accuracy10.csv"), sep=",", index=False)
+df.to_csv(os.path.join("SF_2020-2021/Model_Utility_Folder/Analysis", "B_K2.csv"), sep=",", index=False)
+
+def mean(list):
+    return sum(list)/len(list)
+
+average = mean(listOfAccuracies)
+print(average)
