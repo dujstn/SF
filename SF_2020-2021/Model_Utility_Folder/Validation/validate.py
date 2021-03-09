418031e4cbbf69241c3ff735d4b53f9126ac2620
dujstn <du.justinz@gmail.com>
2021-03-08 22:41:05 -0800

    BREAKEVEN PRICE MODEL + DATA VALIDATION

--- SF_2020-2021/Model_Utility_Folder/Validation/validate.py
+++ SF_2020-2021/Model_Utility_Folder/Validation/validate.py
@@ -3,106 +3,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import math
-from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import GridSearchCV
 import pickle
-
-# CORE MODEL
-class knn():
-    # Mean calculation
-    def mean(self, labels):
-        return sum(labels)/len(labels)
-
-    # Euclidean distance for KNN
-    def euclidDis(self, ptOne, ptTwo):
-        distanceSqrdSum = 0
-        for i in range(len(ptOne)):
-            distanceSqrdSum += math.pow(ptOne[i] - ptTwo[i], 2)
-        return math.sqrt(distanceSqrdSum)
-
-    # Fit data to model
-    def fit(self, data, query):
-        dictionary = []
-        for item in query:
-            distAndIndi = []
-            for index, example in enumerate(data):
-                distance = self.euclidDis(example[:-1], item)
-                distAndIndi.append((distance, index))
-            dictionary.append(distAndIndi)
-        return dictionary
-
-    # Evaluate data and make predictions
-    def predict(self, data, query, k, dictionary):
-        listPredictedChoice = []
-        listPredictedDist = []
-        distAndIndi = dictionary
-
-        for item in query:
-            distAndIndi_sorted = sorted(distAndIndi)
-            distAndIndi_K = distAndIndi_sorted[:k]
-            nearestLabels_K = [data[i][-1] for distance, i in distAndIndi_K]
-
-            listPredictedChoice.append(self.mean(nearestLabels_K))
-            listPredictedDist = listPredictedDist + distAndIndi_K
-
-        return listPredictedChoice
-
+np.set_printoptions(threshold=np.inf)
 
 # Main function
 train = pd.read_csv("SF_2020-2021/Data/TRAIN.csv")
-eval2 = pd.read_csv("SF_2020-2021/Data/EVAL.csv")
+eval = pd.read_csv("SF_2020-2021/Data/EVAL.csv")
 
-feats_TRAIN = train[["Array Type", "Market Pricing", "Utility-scale Tariff Applied", "First Yr Annual (MWh)", "Reference Price ($/MWh)", "Total Capacity (MW)", "Breakeven Price ($/MWh)"]]
-trainList = [list(row) for row in feats_TRAIN.values]
+feats_TRAIN = train[["Array Type", "Market Pricing", "Utility-scale Tariff Applied", "First Yr Annual (MWh)", "Reference Price ($/MWh)", "Total Capacity (MW)"]]
+trainFeatList = [list(row) for row in feats_TRAIN.values]
+label_TRAIN = train[["Breakeven Price ($/MWh)"]]
+trainLabelList = [list(row) for row in label_TRAIN.values]
 
-feats_EVAL = eval2[["Array Type", "Market Pricing", "Utility-scale Tariff Applied", "First Yr Annual (MWh)", "Reference Price ($/MWh)", "Total Capacity (MW)"]]
-evalList = [list(row) for row in feats_EVAL.values]
-
-# Model fitting
-
-output = knn()
-model = output.fit(trainList, evalList)
-print(model)
-
-
-# PICKLE TO SAVE AND LOAD STATES OF MODEL
-filename = "SF_2020-2021/Model_Utility_Folder/Validation/pickle.pkl"
-
-
-with open(filename, "wb") as file:
-    pickle.dump(model, file)
-
-"""
-with open(filename, "rb") as file:
-    model = pickle.load(file)
-"""
-"""
-# Prepping data for evaluation
-x = open("SF_2020-2021/Model_Utility_Folder/Validation/distAndIndi.txt").read()
-output = [list(value) for value in x.split("; ")]
-list = []
-for i in output:
-    list.append("".join(i))
-aux = []
-for value in list:
-    x = value.split(", ")
-    x[0] = float(x[0])
-    x[1] = int(x[1])
-    aux.append(x)
+feats_EVAL = eval[["Array Type", "Market Pricing", "Utility-scale Tariff Applied", "First Yr Annual (MWh)", "Reference Price ($/MWh)", "Total Capacity (MW)"]]
+evalFeatList = [list(row) for row in feats_EVAL.values]
+label_EVAL = eval[["Breakeven Price ($/MWh)"]]
+evalLabelList = [list(row) for row in label_EVAL.values]
+evalLabelList = np.array(evalLabelList)
 
 # Evaluation of datapoints
-model = knn()
-print(aux)
-x = model.predict(trainList, evalList, k=1, dictionary=aux)
-"""
-
-# CV
-"""
-grid = {"k": np.arange(1, 8)}
-model = knn(trainList, evalList, k=1, distanceFN=euclidDis, choiceFN=mean)
-gcsv = GridSearchCV(model, grid, cv=5)
-gcsv.fit(trainList, evalList)
-
-print(gcsv.cv_results_)
-print(gcsv.best_params_)
-"""
+model = KNeighborsRegressor()
+knn_grid = {'n_neighbors': np.arange(1, 10)}
+model_knn = GridSearchCV(model, knn_grid, cv=10)
+model_knn.fit(trainFeatList, trainLabelList)
+print("Best K Value: ", model_knn.best_params_)
+print("R^2 - Training: ", model_knn.best_score_)
+output = model_knn.predict(evalFeatList)
+print("\n\nAverage Error % (Evaluation, K = 1): ", mean_absolute_error(evalLabelList, output))
+print("R^2 - Evaluation: ", model_knn.score(evalFeatList, evalLabelList))
