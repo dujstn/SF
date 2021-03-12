2a7d0048289c55694567a724632af7355ae46750
dujstn <du.justinz@gmail.com>
2021-03-11 22:55:27 -0800

    2nd Iteration

--- SF_2020-2021/Machine_Learning_Model/POWER_PRODUCTION.py
+++ SF_2020-2021/Machine_Learning_Model/POWER_PRODUCTION.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
 np.set_printoptions(threshold=np.inf)
 
 # Data Preparation
@@ -28,5 +29,26 @@ print("R^2 - Training: ", model_knn.best_score_)
 output = model_knn.predict(evalFeatList)
 print("\n\nAverage Error % (Evaluation, K = 1): ", mean_absolute_error(evalLabelList, output))
 print("R^2 - Evaluation: ", model_knn.score(evalFeatList, evalLabelList))
+print(model_knn.cv_results_)
 
-print(output)
+p = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+zero = model_knn.cv_results_["split0_test_score"]
+one = model_knn.cv_results_["split1_test_score"]
+two = model_knn.cv_results_["split2_test_score"]
+three = model_knn.cv_results_["split3_test_score"]
+four = model_knn.cv_results_["split4_test_score"]
+five = model_knn.cv_results_["split5_test_score"]
+six = model_knn.cv_results_["split6_test_score"]
+seven = model_knn.cv_results_["split7_test_score"]
+eight = model_knn.cv_results_["split8_test_score"]
+nine = model_knn.cv_results_["split9_test_score"]
+splits = [zero, one, two, three, four, five, six, seven, eight, nine]
+
+for i in range(len(splits)):
+    plt.plot(p, splits[i], '-o', label= "Number of Splits: " + str(i))
+
+plt.xlabel("K Value", fontsize=30)
+plt.ylabel("R-Squared Value", fontsize=30)
+plt.title("R-Squared Values - Power Production (Various Splits)", fontsize = 30)
+plt.legend()
+plt.show()
